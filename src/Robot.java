fb8453663ef51a2a533521bc86e2597eaf5deb9b
nazarperkatyuk <perkatyuk.n@gmail.com>
2019-10-23 17:25:45 +0300

    commit removing

--- src/Robot.java
+++
@@ -1,7 +0,0 @@
-public class Robot {
-    public void work(){
-        System.out.println("Я Robot - я просто працюю");
-    }
-}
-
-
--- src/RobotDancer.java
+++
@@ -1,6 +0,0 @@
-public class RobotDancer {
-    public void work(){
-        System.out.println("Я RoborDance - я просто танцюю");
-    }
-}
-
