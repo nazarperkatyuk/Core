ce5e32fb7e16a83689d0b1bba6ce5f295917cb61
nazarperkatyuk <perkatyuk.n@gmail.com>
2019-10-24 18:47:34 +0300

    commit

---
+++ src/Helm.java
@@ -0,0 +1,24 @@
+public class Helm {
+
+    private int diametr = 30;
+
+    public Helm(int  diametr) {
+        this.diametr = diametr;
+    }
+    public Helm(){}
+
+    public int getDiametr () {
+        return diametr; }
+    public void setDiametr ( int diametr){
+        this.diametr += diametr;
+    }
+
+    @Override
+    public String toString() {
+        return "Helm{" + "diameter = " + diametr +'}';
+    }
+    public void changeDiameter(){
+        diametr += 2;
+    }
+}
+
