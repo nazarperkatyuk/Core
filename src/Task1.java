f9678044b3447853a2d0c5671a91941a93d82154
nazarperkatyuk <perkatyuk.n@gmail.com>
2019-11-29 16:07:37 +0200

    commit remmov

---
+++ src/Task1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        addString(list);
+        System.out.println(list);
+
+    }
+
+    public static void addString(List<Integer> list) {
+
+    }
+}
